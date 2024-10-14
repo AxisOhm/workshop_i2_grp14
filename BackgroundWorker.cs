29eb1a32c0b834a647de934cdb6b803ac81fd629
BASSOLET Gary <ga.bassolet@gmail.com>
2024-10-14 15:26:57 +0200

    Initial commit

---
+++ BackgroundWorker.cs
@@ -0,0 +1,23 @@
+﻿using Blazor.BrowserExtension;
+using System.Threading.Tasks;
+
+namespace workshop_i1_grp14
+{
+    public partial class BackgroundWorker : BackgroundWorkerBase
+    {
+        [BackgroundWorkerMain]
+        public override void Main()
+        {
+            WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+        }
+
+        async Task OnInstalled()
+        {
+            var indexPageUrl = WebExtensions.Runtime.GetURL("index.html");
+            await WebExtensions.Tabs.Create(new()
+            {
+                Url = indexPageUrl
+            });
+        }
+    }
+}
